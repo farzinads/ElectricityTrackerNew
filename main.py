94f50aeaa353301e6300582a57666445f4899a50
farzinads <farzinahmadi1360@gmail.com>
2025-03-24 10:46:06 +0100

    update project

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from PyQt5.QtWidgets import QApplication
 import sys
+from PyQt5.QtWidgets import QApplication
 from gui.vertragsregistrierung import VertragsRegistrierung
+import os
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = VertragsRegistrierung(db_path="C:/Projects/Stromverbrauch-Rechner/contracts.db")
+    # مسیر دیتابیس رو نسبت به محل فایل main.py تنظیم می‌کنیم
+    db_path = os.path.join(os.path.dirname(__file__), "contracts.db")
+    window = VertragsRegistrierung(db_path=db_path)
     window.show()
     sys.exit(app.exec_())
